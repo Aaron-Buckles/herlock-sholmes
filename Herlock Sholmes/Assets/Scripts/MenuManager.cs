df29dc14610d3e20e07f41672ddc1e7faa942013
Aaron Buckles <aaronjbuckles@outlook.com>
2017-12-10 10:33:00 -0800

    Added basic menues

---
+++ Herlock Sholmes/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    [Tooltip("Element 0 should be the main menu")]
+    public GameObject[] menues;
+
+
+	void Start()
+    {
+        OpenMenu(0);
+    }
+
+
+    public void OpenMenu(int menuIndex)
+    {
+        foreach(GameObject menu in menues)
+        {
+            menu.SetActive(false);
+        }
+
+        menues[menuIndex].SetActive(true);
+    }
+
+}
