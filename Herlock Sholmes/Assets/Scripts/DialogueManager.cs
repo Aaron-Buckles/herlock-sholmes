7f7960d31bca55537b5c33c832bb8bb0ac627ee1
Aaron Buckles <aaronjbuckles@outlook.com>
2017-12-11 20:42:09 -0800

    Dialogue UI animations

---
+++ Herlock Sholmes/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Animator animator;
+
+    bool speaking = false;
+
+
+    public void TestButtonClicked()
+    {
+        speaking = !speaking;
+        UpdateDialoguePanel();
+    }
+
+    
+    private void UpdateDialoguePanel()
+    {
+        animator.SetBool("speaking", speaking);
+    }
+        
+
+}
