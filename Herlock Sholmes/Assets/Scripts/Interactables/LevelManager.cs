feee034eda0faf5890bb0d1fa792914c41cb2c97
Aaron-Buckles <aaronjbuckles@outlook.com>
2018-05-16 14:35:00 -0700

    Added level complete menu

---
+++ Herlock Sholmes/Assets/Scripts/Interactables/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : Interactable
+{
+
+    public GameObject nextLevelPanel;
+    public string sceneName;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        nextLevelPanel.SetActive(false);
+    }
+
+    public override void PerformAction()
+    {
+        Cursor.visible = true;
+        nextLevelPanel.SetActive(true);
+    }
+
+    public override void UnPerformAction()
+    {
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void NextButton()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
