2f5e89a506da6ae3053d3f47f0725fd4126c66af
Aaron-Buckles <aaronjbuckles@outlook.com>
2018-05-16 19:05:23 -0700

    Brian and Camden's mechanics

---
+++ Herlock Sholmes/Assets/Scripts/Collectable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectType
+{
+    Star,
+    Other
+};
+
+public class Collectable : MonoBehaviour
+{
+    public ObjectType type;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            collect();
+        }
+    }
+
+    private void collect()
+    {
+        switch (type)
+        {
+            case ObjectType.Star:
+                gameObject.SetActive(false);
+                Debug.Log("Collect a star");
+                break;
+
+            case ObjectType.Other:
+                gameObject.SetActive(false);
+                Debug.LogError("Unknown type of collectable");
+                break;
+        }
+    }
+}
---
+++ Herlock Sholmes/Assets/Scripts/Triggers/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : PressurePlate
+{
+
+    public GameObject destination;
+
+    GameObject player;
+
+    public override void OnTriggerEnter2D(Collider2D col)
+    {
+        player = col.gameObject;
+        triggered = true;
+        player.transform.position = destination.transform.position;
+    }
+
+    public override void OnTriggerExit2D(Collider2D col)
+    {
+        triggered = false;
+    }
+}
